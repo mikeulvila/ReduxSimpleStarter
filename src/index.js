29c4e6d8e08f15167adeda10825ee65f2fe7861f
Mike Ulvila <mikeulvila@me.com>
2016-06-30 17:04:54 -0500

    video detail properly displays in dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
+import VideoDetail from './components/video_detail.js';
 import VideoList from './components/video_list.js'
 import API_KEY from './API_KEY.js';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
         <div>
           <SearchBar />
+          <VideoDetail video={this.state.videos[0]}/>
           <VideoList videos={this.state.videos} />
         </div>
       );
