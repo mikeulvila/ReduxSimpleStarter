f9893545673c44060313b281b125885c25d259e4
Mike Ulvila <mikeulvila@me.com>
2016-06-30 17:24:04 -0500

    pass onVideoSelect function all the way down the chain to onClick of <li> tags

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     // youtube api call
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -23,8 +29,10 @@ class App extends Component {
     return (
         <div>
           <SearchBar />
-          <VideoDetail video={this.state.videos[0]}/>
-          <VideoList videos={this.state.videos} />
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList
+            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+            videos={this.state.videos} />
         </div>
       );
   };
