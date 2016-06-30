bf96bfe1933d1a511002738765879391aac5b8c7
Mike Ulvila <mikeulvila@me.com>
2016-06-30 14:47:24 -0500

    add constructor function to SearchBar class.  calling the parent Component constructor function with super(props)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input />
+    return <input onChange={this.onInputChange} />
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
